a6d3099079c4c1f88c75d9f99e80e555f71ba3c2
lucasound22 <lucamarino78@gmail.com>
2026-08-20 09:28:27 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='dj-luca-v12';self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
